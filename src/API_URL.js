cd3034386e9b184415e5776e3aaedc44d4e675a7
Matheus <matheus12bh@gmail.com>
2021-07-07 16:27:00 -0300

    add-dev-config

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,3 +1,3 @@
 //export const API_URL = "https://api.cotamaq.com.br/api/v1"
 //export const API_URL = "http://54.232.157.3/api/v1"
-//export const API_URL = "https://homologacao.cotamaq.com.br/api/v1"
+export const API_URL = "https://homologacao.cotamaq.com.br/api/v1"
