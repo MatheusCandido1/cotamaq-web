307d824c10e339d2ecffd64c7d9ace0906b64f99
Matheus <matheus12bh@gmail.com>
2021-07-12 17:14:36 -0300

    update-url

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ window.Echo = new Echo({
      broadcaster: 'pusher',
      key: process.env.VUE_APP_PUSHER_APP_KEY,
      cluster: process.env.VUE_APP_PUSHER_APP_SECRET,
-     wsHost: '127.0.0.1',
+     wsHost: 'plataforma.cotamaq.com.br',
      wsPort: 6001,
      forceTLS: false,
      disableStats: true
