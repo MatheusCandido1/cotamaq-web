cf4ec9b4309991ecef2349e24b0f488b8f823c5f
Matheus <matheus12bh@gmail.com>
2021-07-21 21:00:48 -0300

    add-efect

--- src/components/Buyer/Products/ProductShow.vue
+++ src/components/Buyer/Products/ProductShow.vue
@@ -3,7 +3,7 @@
       <div  class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
       <div class="relative w-auto my-6 mx-auto max-w-6xl">
         <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div class="absolute inset-0 bg-gray-900 opacity-90"></div>
+          <div class="absolute inset-0 bg-gray-900 opacity-80"></div>
         </div>
         <!--content-->
         <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
