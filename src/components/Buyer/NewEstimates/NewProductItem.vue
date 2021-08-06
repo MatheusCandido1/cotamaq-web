a2d37cd477d5130ec3560b84f952bcceddb08046
Matheus <matheus12bh@gmail.com>
2021-08-06 12:42:26 -0300

    add-new-product

---
+++ src/components/Buyer/NewEstimates/NewProductItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="relative bg-white   overflow-hidden rounded-xl w-64 my-4 shadow-xl">
+        <div class="cursor-pointer flex flex-col min-h-full bg-primary-main justify-center items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            <span class="font-semibold text-white mb-2">Nova Cotação</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NewProductItem',
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+
+}
+</script>
+
+<style>
+
+</style>
