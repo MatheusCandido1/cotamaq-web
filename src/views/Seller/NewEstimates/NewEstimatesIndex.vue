adf1b1d640e57f64b6171170906615b5d6d6528c
Matheus <matheus12bh@gmail.com>
2021-08-19 09:36:13 -0300

    add-componente

---
+++ src/views/Seller/NewEstimates/NewEstimatesIndex.vue
@@ -0,0 +1,31 @@
+<template>
+   <span>
+       <div class="my-6 px-2 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <div class="flex">
+                <div class="w-full px-3 mb-5">
+                    <h2 class="text-3xl font-semibold text-center text-primary-main dark:text-gray-200">
+                        Suas Cotações [Vendedor]
+                    </h2>
+                </div>
+            </div>
+
+            <div class="flex">
+                <div class="w-full px-3 mb-5">
+                    <input type="text" placeholder="Pesquisar..." class="w-full pl-2 pr-3 py-2 rounded border-b-2 border-primary-main shadow-md py-2 px-6 outline-none focus:border-primary-lighter" />
+                </div>
+            </div>
+                    
+        </div>
+    </span>
+</template>
+<script>
+export default {
+    name: 'NewEstimateIndex',
+    components: {
+    },
+}
+</script>
+
+<style>
+
+</style>
