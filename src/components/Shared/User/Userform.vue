35f9d481db4f1374250afeac8c4a54ec08191726
nicholas de paula <cplatinado@gmail.com>
2021-08-20 19:07:22 -0300

    validate-form-user

--- src/components/Shared/User/Userform.vue
+++ src/components/Shared/User/Userform.vue
@@ -6,28 +6,28 @@
             </div>
         </div>
 
-        <form @submit.prevent="createUser()">
+        <form @submit.prevent="createUser">
              <div class="lg:flex">
                    <div class="lg:w-1/3 md:w-full  sm:w-full px-3 mb-5">
                     <label for="" class="text-sm font-semibold text-gray-600 px-1">Nome</label>
                     <div class="flex">
-                        <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
-                        <input placeholder="Ex: Fernando"  v-model="user.name"     type="text" class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md px-6 outline-none  border-primary-lighter">
-                    </div>  
-                    <!-- <div v-if="errors.name == 'ERROR'">
+                        <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center" ></div>
+                        <input placeholder="Ex: Fernando"  v-model="user.name "  @change="() => (errors.user.name = 'OK')" :class="errors.user.name == 'ERROR' ? 'border-red-400':'border-primary-main'"   type="text" class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md px-6 outline-none  ">
+                    </div> 
+                    <div v-if="errors.user.name == 'ERROR'">
                         <span class="text-xs text-red-400 font-semibold px-1">O campo Nome é obrigatório.</span>
-                    </div>                        -->
+                    </div>                       
                 </div>
 
                  <div class="lg:w-1/3 md:w-full  sm:w-full px-3 mb-5">
                     <label for="" class="text-sm font-semibold text-gray-600 px-1">Email</label>
                     <div class="flex">
                         <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
-                        <input placeholder="Ex: fernando@gmail.com"  v-model="user.email"     type="text" class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md px-6 outline-none  border-primary-lighter">
-                    </div>  
-                    <!-- <div v-if="errors.email == 'ERROR'">
+                        <input placeholder="Ex: fernando@gmail.com"  v-model="user.email"  @change="() => (errors.user.email = 'OK')" :class="errors.user.email == 'ERROR' ? 'border-red-400':'border-primary-main'"    type="text" class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md px-6 outline-none  ">
+                    </div>   
+                    <div v-if="errors.user.email == 'ERROR'">
                         <span class="text-xs text-red-400 font-semibold px-1">O campo Email é obrigatório.</span>
-                    </div>                        -->
+                    </div>                       
                 </div>
 
 
@@ -35,11 +35,11 @@
                     <label for="" class="text-sm font-semibold text-gray-600 px-1">Senha</label>
                     <div class="flex">
                         <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
-                        <input placeholder="Digite sua senha" v-model="user.password"    type="password" class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md px-6 outline-none  border-primary-lighter">
+                        <input placeholder="Digite sua senha" v-model="user.password"  @change="() => (errors.user.password = 'OK')" :class="errors.user.password == 'ERROR' ? 'border-red-400':'border-primary-main'"    type="password" class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md px-6 outline-none ">
                     </div>  
-                    <!-- <div v-if="errors.password == 'ERROR'">
+                    <div v-if="errors.user.password == 'ERROR'">
                         <span class="text-xs text-red-400 font-semibold px-1">O campo Senha é obrigatório.</span>
-                    </div>                        -->
+                    </div>                       
                 </div>
              </div>
 
@@ -61,6 +61,7 @@
 <script>
 import { companyService} from '../../../services'
 import { bus } from '../../../main';
+import { required } from 'vuelidate/lib/validators'
 
 
     export default {
@@ -71,13 +72,32 @@ import { bus } from '../../../main';
                     email:'',
                     password:'',
                 },
-                disabled:false
+                disabled:false,
+                errors:{
+                    user:{
+                        name:null,
+                        email:null,
+                        password:null,
+                    }
+                }
             }
         },
         methods: {
             createUser() {
+                this.$v.$touch()
+              
+                if(this.$v.user.name.$invalid){
+                    this.errors.user.name = 'ERROR'                  
+                } 
+                if(this.$v.user.email.$invalid){
+                    this.errors.user.email = 'ERROR'                  
+                }
+                 if(this.$v.user.password.$invalid){
+                    this.errors.user.password = 'ERROR'                  
+                }
 
-               companyService.createUser(this.user).then((response)=>{
+                if(this.$v.$anyError == false) {
+                 companyService.createUser(this.user).then((response)=>{
                    this.$toast.success(response.success_message, {
                     position: "bottom-right",
                     pauseOnHover: false,
@@ -86,9 +106,28 @@ import { bus } from '../../../main';
                 });
                 bus.$emit('updatedUser', true);
                })
+            }
+
+            
                
             }
         },
+        validations: {
+            user:{
+                name:{
+                   required
+                },
+                email:{
+                    required
+                },
+                password:{
+                    required
+                }
+                  
+                  
+            }
+              
+        }
       
     }
 </script>
