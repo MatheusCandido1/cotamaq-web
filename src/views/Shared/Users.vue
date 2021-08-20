fded0f00b349c93e9780632e572ff059a5509c88
nicholas de paula <cplatinado@gmail.com>
2021-08-20 10:47:30 -0300

    create-user-view

---
+++ src/views/Shared/Users.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <UserForm></UserForm>
+    </div>
+</template>
+
+<script>
+import  UserForm from '../../components/Shared/User/Userform.vue'
+    export default {
+        components:{
+            UserForm,
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
