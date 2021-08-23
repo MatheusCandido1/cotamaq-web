750dc88b77596180ea90bf4a0c175dd3a9d17c2e
Matheus <matheus12bh@gmail.com>
2021-08-23 19:50:05 -0300

    add-card

--- src/views/Seller/Proposals/ProposalIndex.vue
+++ src/views/Seller/Proposals/ProposalIndex.vue
@@ -1,12 +1,18 @@
 <template>
-    <ProposalForm />
+    <div>
+        <ProposalGrid />
+        <ProposalForm />
+    </div>
 </template>
 
 <script>
+import ProposalGrid from '../../../components/Seller/Proposals/ProposalGrid'
 import ProposalForm from '../../../components/Seller/Proposals/ProposalForm'
+
 export default {
     name: 'ProposalIndex',
     components: {
+        ProposalGrid,
         ProposalForm
     }
 
