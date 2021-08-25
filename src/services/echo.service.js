ba03d2346883ad7d2f4a118160f5cd5d759dd4bf
Matheus <matheus12bh@gmail.com>
2021-08-25 12:57:10 -0300

    add-service

--- src/services/echo.service.js
+++ src/services/echo.service.js
@@ -7,7 +7,7 @@ export const echoService = {
 export function connect() {
     
 var token = sessionStorage.getItem('token')
-
+console.log('executou')
 window.Echo = new Echo({
   broadcaster: "pusher",
   key: "83cb60f1b6a7a39928a5",
