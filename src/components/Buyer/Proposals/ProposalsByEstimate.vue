32270efd54cd7d4e83fb1f343e1f111295ed3944
nicholas de paula <cplatinado@gmail.com>
2021-08-28 19:53:19 -0300

    make-proposal-sort

--- src/components/Buyer/Proposals/ProposalsByEstimate.vue
+++ src/components/Buyer/Proposals/ProposalsByEstimate.vue
@@ -9,11 +9,11 @@
           </h2>
         </div>
       </div>
-      <div class="flex lg:px-3 flex-row justify-between">
-        <div class="py-1">
-          <span @click="showEstimateModal" class="items-center justify-center px-2 py-1 text-md font-bold rounded-md text-white bg-primary-main cursor-pointer">Cotação #{{estimate.id}} - {{estimate.description}}<i class="mdi mdi-file-search ml-2"></i></span>
+      <div class="flex flex-wrap lg:px-3 flex-row justify-between">
+        <div class="py-1 md:w-auto w-full">
+          <span @click="showEstimateModal" class=" items-center justify-center px-2 py-1 text-md font-bold rounded-md text-white bg-primary-main cursor-pointer">Cotação #{{estimate.id}} - {{estimate.description}}<i class="mdi mdi-file-search ml-2"></i></span>
         </div>
-        <div class="py-1">
+        <div class="py-1 md:w-auto w-full">
           <span @click="showEquipmentModal" class="items-center justify-center px-2 py-1 text-md font-bold text-white bg-primary-main rounded-md cursor-pointer">Detalhes do Equipamento<i class="mdi mdi-file-search ml-2"></i></span>
         </div>
       </div>
@@ -27,7 +27,7 @@
       <div class="border-t-2 lg:px-3 "></div>
       <div class="flex flex-row justify-start gap-2 mt-2 lg:px-3 ">
             <button  :class="formatItem(1).bg" class="text-white text-sm font-semibold text-md px-4 py-1 rounded-md mb-2">{{formatItem(1).text}}<i class="mdi mdi-sort ml-2"></i></button>
-            <button  @click="handleSortPrice" :class="formatItem(2).bg" class="text-white text-sm font-semibold text-md px-4 py-1 rounded-md mb-2">{{formatItem(2).text}}<i class="mdi mdi-sort ml-2"></i></button>
+            <button  @click="handleSortPrice" :class="formatItem(2).bg"  class="text-white text-sm font-semibold text-md px-4 py-1 rounded-md mb-2">{{formatItem(2).text}}<i class="mdi ml-2" :class="sortPrice"></i></button>
             <button  :class="formatItem(3).bg" class="text-white text-sm font-semibold text-md px-4 py-1 rounded-md mb-2">{{formatItem(3).text}}<i class="mdi mdi-sort ml-2"></i></button>
             <button  :class="formatItem(4).bg" class="text-white text-sm font-semibold text-md px-4 py-1 rounded-md mb-2">{{formatItem(4).text}}<i class="mdi mdi-sort ml-2"></i></button>
            </div>
@@ -35,6 +35,7 @@
         <div class="w-full lg:px-3 lg:mb-5 xl:px-3 xl:mb-5">
           <div class="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-x-6">
             <ProposalPartItem v-for="proposal in proposals" :key="proposal.id" :estimate="estimate"  :proposal="proposal" />
+            <!-- <ProposalPartItem v-for="proposal in sortDesc" :key="proposal.id" :estimate="estimate"  :proposal="proposal" /> -->
           </div>
         </div>
       </div>
@@ -71,8 +72,49 @@ export default {
             }
         })
     },
+   computed: {
+     sortPrice(){
+       return {'mdi-sort': this.sorting.price.default, 'mdi-sort-ascending': this.sorting.price.ascending, 'mdi-sort-descending': this.sorting.price.descending }
+     },
+     sortAsc: function () {
+       let list  = this.proposals
+       list = list.sort((a,b)=>{
+         if(a.total < b.total){
+           return -1
+         }
+         if(a.total < b.total){
+           return 1
+         }
+         return 0
+       })
+
+       return list
+     },
+     sortDesc: function () {
+       let list  = this.proposals
+       list = list.sort((a,b)=>{
+         if(a.total > b.total){
+           return -1
+         }
+         if(a.total < b.total){
+           return 1
+         }
+         return 0
+       })
+
+       return list
+     }
+        
+   },   
     data() {
         return {
+         sorting:{
+           price:{
+              descending:false,
+              ascending:false,
+              default:true,
+           }
+         },
           modal: {
             equipment: false,
             estimate: false,
@@ -88,30 +130,49 @@ export default {
             price: 'asc'
           },
           estimate: {},
-          proposals: []
+          proposals: [],
+          proposalsDefaults:[]
         }
     },
     methods: {
       handleSortPrice() {
         if(this.sort.price == 'default') {
-          // Ordenação padrão
-          console.log('1')
+          this.sorting.price.default = true
+          this.sorting.price.ascending = false
+          this.sorting.price.descending = false
+           
+          
+          this.proposals = this.proposalsDefaults         
+          this.sort.price = 'asc'
+          
+
+        }else if(this.sort.price == 'asc') {
+            this.sorting.price.ascending = true
+            this.sorting.price.default = false
+            this.sorting.price.descending= false
+
+            this.proposals = this.sortAsc
+
+           this.sort.price = 'desc'
+        }else if(this.sort.price == 'desc') {
+          this.sorting.price.descending = true
+          this.sorting.price.default = false
+          this.sorting.price.ascending = false
+
+          this.proposals = this.sortDesc
+          this.sort.price = 'default'
+
+    
         }
-        if(this.sort.price == 'asc') {
-          // Menor preço para o maior
-          console.log('1')
-        }
-        if(this.sort.price == 'desc') {
-          // Maior preço para o menor
-          console.log('1')
-        }
-        
       },
       getProposalsByEstimate() {
         const estimate_id = this.$route.params.estimate_id
         estimateService.getEstimate(estimate_id).then((response) => {
           this.estimate = response.data.data
           this.proposals = response.data.data.proposals
+         
+          this.proposalsDefaults = response.data.data.proposals
+          localStorage.setItem('proposal',JSON.stringify(response.data.data.proposals))
         }).catch((error) => {
           console.log(error.response)
         })
