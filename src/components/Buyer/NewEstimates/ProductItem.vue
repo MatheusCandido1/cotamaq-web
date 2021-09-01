14c6b2a1daee8600ea8dedf7470f7aa9771f18ff
MatheusCandido1 <matheus12bh@gmail.com>
2021-09-01 16:31:07 -0300

    add-order-redirect

--- src/components/Buyer/NewEstimates/ProductItem.vue
+++ src/components/Buyer/NewEstimates/ProductItem.vue
@@ -64,7 +64,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
                         </svg>
                     </button>
-                    <button v-tooltip="{ content: 'Pedidos' }" v-if="estimate.status == 4" class="flex items-center justify-between px-2 py-2 bg-indigo-600 text-sm font-medium leading-5 text-white rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+                    <button @click="handleOrderClick" v-tooltip="{ content: 'Pedidos' }" v-if="estimate.status == 4" class="flex items-center justify-between px-2 py-2 bg-indigo-600 text-sm font-medium leading-5 text-white rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
                         </svg>
@@ -122,6 +122,9 @@ export default {
         formatMissingInformation,
         formatEquipment,
         formatSimillar,
+        handleOrderClick() {
+            this.$router.push({name: 'orders'})
+        },
         handleProposalClick() {
             this.$router.push({name: 'ProposalsByEstimate', params: {estimate_id: this.estimate.id}})
         },
