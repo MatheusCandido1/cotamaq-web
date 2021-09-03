20dec7df80b9426e7527a8c42b8f048f2e9dfd28
nicholas de paula <cplatinado@gmail.com>
2021-09-03 12:48:41 -0300

    BugFix-refuse-proposal

--- src/components/Seller/NewEstimates/ProductItem.vue
+++ src/components/Seller/NewEstimates/ProductItem.vue
@@ -80,7 +80,7 @@
 
                         <div v-if="formatStatus().id == 2" class="flex items-center space-x-1 text-sm">
                             <button
-                                @click="handlePropsalsBySellerClick"
+                                @click="handleProposalsBySellerClick"
                                 class="flex items-center justify-between px-2 py-2 bg-primary-main text-sm font-medium leading-5 text-white rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
                                <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"/>
