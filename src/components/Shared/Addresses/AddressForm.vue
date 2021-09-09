699da8920d2ef7b1694236ab047d6cc47844da8d
Pedro Arthur <pedro13arthur@gmail.com>
2021-09-09 13:39:27 -0300

    fix-address-bug

--- src/components/Shared/Addresses/AddressForm.vue
+++ src/components/Shared/Addresses/AddressForm.vue
@@ -85,19 +85,18 @@
             <div class="lg:w-1/4 md:w-1/2 w-full px-3 mb-5">
                 <label for="" class="text-sm font-semibold text-gray-600 px-1">Número</label>
                 <div class="flex flex-wrap items-stretch w-full mb-4 relative">
-			<input  @focusout="() => (errors.number = 'OK')"   v-model="address.number" :class="errors.number == 'ERROR' ? 'border-red-400':'border-primary-main'"  @change="numberless = false" type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 rounded-l border-b-2 shadow-md py-2 px-6 outline-none  focus:border-primary-lighter px-3 relative">
-			<div class="flex -mr-px">
-				<span :class="errors.number == 'ERROR' ? 'border-red-400':'border-primary-main'" class="flex items-center leading-normal bg-grey-lighter rounded-r border-b-2 shadow-md border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">
-                    <label class="inline-flex items-center">
-                    <input v-if="numberless != null "  @focusout="() => (errors.number = 'OK')" v-model="numberless" @change="address.number = null" type="checkbox"  class="form-radio text-green-500">
-                    <span @click="setSN"  class="ml-3 text-lg">S/N</span>
-                </label>
-                </span>
-			</div>
-                <div  v-if="errors.number == 'ERROR'">
-                    <span class="text-xs text-red-400 font-semibold px-1">O campo Número é obrigatório.</span>
-                </div>  
-		</div>
+                    <input @focusout="() => (errors.number = 'OK')"   v-model="address.number" :class="errors.number == 'ERROR' ? 'border-red-400':'border-primary-main'"  @change="numberless = false" type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 rounded-l border-b-2 shadow-md py-2 px-6 outline-none  focus:border-primary-lighter px-3 relative">
+                    <div class="flex -mr-px">
+                        <span :class="errors.number == 'ERROR' ? 'border-red-400':'border-primary-main'" class="flex items-center leading-normal bg-grey-lighter rounded-r border-b-2 shadow-md border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">
+                        <label class="inline-flex items-center">
+                        <span @click="setSN" class="ml-3 cursor-pointer text-lg">S/N</span>
+                        </label> 
+                        </span>  
+                    </div>	
+                    <div  v-if="errors.number == 'ERROR'">
+                        <span class="text-xs text-red-400 font-semibold px-1">O campo Número é obrigatório.</span>
+                    </div>  
+                </div>    
             </div>
 
 
@@ -112,16 +111,16 @@
         </div>
 
         <div class="flex justify-end -mx-3">
-            <div class=" px-3 mb-5">
+            <div  class="md:w-1/4 w-1/3 px-3 mb-5">
                 <label for="" class="text-sm font-semibold text-gray-600 px-1"></label>
-                <div class="flex">
-                <label class="inline-flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" :class="{'text-primary-main ': address.main}" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                    </svg>
-                    <span @click="setMain" class="ml-3 text-sm ">Endereço padrão</span>
-                </label>    
-                </div>                         
+                    <div class="flex">
+                        <label @click="setMain" class="inline-flex items-center cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="md:h-6 md:w-6  h-10 w-10  " :class="{'text-green-500':address.main}" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                        </svg>
+                        <span  class="ml-3 text-lg">Endereço padrão</span>
+                        </label>    
+                    </div>                         
             </div>
 
             <div class="w-1/4 px-3 mb-5">
@@ -212,13 +211,12 @@ export default {
     created() {
     },
     methods: {
-      setMain(){
-        this.address.main =  !this.address.main
-      },
-      setSN(){
-        this.address.number= 'sem numero'
-
-      },
+        setSN(){
+            this.address.number = 'Sem Número'
+        },
+        setMain(){
+            this.address.main = !this.address.main
+        },
         async fillAddress() {
             if(this.address.zipcode == ''){
                 this.errors.zipcode = 'ERROR'
