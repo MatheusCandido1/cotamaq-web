3a9eb49882bb3f2e76c8f73086aa116af950c7ae
Pedro Arthur <pedro13arthur@gmail.com>
2021-10-05 10:41:13 -0300

    fix-bug

--- src/components/Shared/Companies/CompanyForm.vue
+++ src/components/Shared/Companies/CompanyForm.vue
@@ -20,40 +20,11 @@
             <label for="" class="text-xs font-semibold px-1"></label>
             <div class="flex" v-if="!edit">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <button
                 @click="enableEdit()"
-                class="
-                  md:w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  flex
-                  items-center
-                  justify-center
-                  bg-primary-main
-                  text-white
-                  font-semibold
-                  rounded
-                  border-b-2 border-primary-main
-                  hover:text-white
-                  shadow-md
-                  py-2
-                  px-6
-                  inline-flex
-                  items-center
-                "
+                class="md:w-full -ml-10 pl-2 pr-3 py-2  flex items-center justify-center bg-primary-main text-white font-semibold rounded border-b-2 border-primary-main  hover:text-white shadow-md py-2 px-6 inline-flex items-center"
               >
                 <span class="justify-center">Editar</span>
               </button>
@@ -67,16 +38,7 @@
             >
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <input
                 :class="
@@ -89,20 +51,7 @@
                 v-model="company.fantasy_name"
                 placeholder=""
                 type="text"
-                class="
-                  w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  rounded
-                  border-b-2
-                  shadow-md
-                  py-2
-                  px-6
-                  outline-none
-                  focus:border-primary-lighter
-                "
+                class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md py-2 px-6 outline-none  focus:border-primary-lighter"
               />
             </div>
             <div v-if="errors.fantasy_name == 'ERROR'">
@@ -112,23 +61,14 @@
             </div>
           </div>
         </div>
-        <div class="md:flex md:flex-wrap -mx-3">
+        <div class="md:flex  md:flex-wrap -mx-3">
           <div class="lg:w-1/4 md:w-1/2 sm:w-full px-3 mb-5">
             <label for="" class="text-sm font-semibold text-gray-600 px-1"
               >Nome ou Razão Social</label
             >
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <input
                 :class="
@@ -141,20 +81,7 @@
                 v-model="company.company_name"
                 placeholder=""
                 type="text"
-                class="
-                  w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  rounded
-                  border-b-2
-                  shadow-md
-                  py-2
-                  px-6
-                  outline-none
-                  focus:border-primary-lighter
-                "
+                class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md py-2 px-6 outline-none  focus:border-primary-lighter"
               />
             </div>
             <div v-if="errors.company_name == 'ERROR'">
@@ -169,16 +96,7 @@
             >
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <the-mask
                 @change.native="() => (errors.cnpj = 'OK')"
@@ -192,20 +110,7 @@
                 v-model="company.cnpj"
                 placeholder=""
                 type="text"
-                class="
-                  w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  rounded
-                  border-b-2
-                  shadow-md
-                  py-2
-                  px-6
-                  outline-none
-                  focus:border-primary-lighter
-                "
+                class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md py-2 px-6 outline-none  focus:border-primary-lighter"
               ></the-mask>
             </div>
             <div v-if="errors.cnpj == 'ERROR'">
@@ -220,16 +125,7 @@
             >
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <input
                 :disabled="!edit"
@@ -237,20 +133,7 @@
                 v-model="company.state_vat"
                 placeholder=""
                 type="text"
-                class="
-                  w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  rounded
-                  border-b-2 border-primary-main
-                  shadow-md
-                  py-2
-                  px-6
-                  outline-none
-                  focus:border-primary-lighter
-                "
+                class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 border-primary-main shadow-md py-2 px-6 outline-none  focus:border-primary-lighter"
               />
             </div>
           </div>
@@ -260,16 +143,7 @@
             >
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <input
                 :disabled="!edit"
@@ -277,20 +151,7 @@
                 v-model="company.city_vat"
                 placeholder=""
                 type="text"
-                class="
-                  w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  rounded
-                  border-b-2 border-primary-main
-                  shadow-md
-                  py-2
-                  px-6
-                  outline-none
-                  focus:border-primary-lighter
-                "
+                class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 border-primary-main shadow-md py-2 px-6 outline-none  focus:border-primary-lighter"
               />
             </div>
           </div>
@@ -302,16 +163,7 @@
             >
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <the-mask
                 :class="
@@ -325,20 +177,7 @@
                 v-model="company.phone"
                 placeholder=""
                 type="text"
-                class="
-                  w-full
-                  -ml-10
-                  pl-2
-                  pr-3
-                  py-2
-                  rounded
-                  border-b-2
-                  shadow-md
-                  py-2
-                  px-6
-                  outline-none
-                  focus:border-primary-lighter
-                "
+                class="w-full -ml-10 pl-2 pr-3 py-2 rounded border-b-2 shadow-md py-2 px-6 outline-none  focus:border-primary-lighter"
               ></the-mask>
             </div>
             <div v-if="errors.phone == 'ERROR'">
@@ -348,41 +187,16 @@
             </div>
           </div>
         </div>
-        <div class="flex justify-end -mx-3 -ml-10 pl-2 pr-3 py-2" v-if="edit">
+        <div class="flex justify-end -mx-3 -ml-10 pl-2 pr-3 py-2 " v-if="edit">
           <div class="w-1/6 mb-5">
             <label for="" class="text-xs font-semibold px-1"></label>
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <button
                 type="submit"
-                class="
-                  w-full
-                  flex
-                  items-center
-                  justify-center
-                  bg-primary-main
-                  text-white
-                  font-semibold
-                  rounded
-                  hover:bg-primary-darker
-                  hover:text-white
-                  shadow-md
-                  py-2
-                  px-6
-                  inline-flex
-                  items-center
-                "
+                class="w-full flex items-center justify-center bg-primary-main text-white font-semibold rounded hover:bg-primary-darker hover:text-white shadow-md py-2 px-6 inline-flex items-center"
               >
                 <span class="justify-center">Salvar</span>
               </button>
@@ -393,36 +207,11 @@
             <label for="" class="text-xs font-semibold px-1"></label>
             <div class="flex">
               <div
-                class="
-                  w-10
-                  z-10
-                  pl-1
-                  text-center
-                  pointer-events-none
-                  flex
-                  items-center
-                  justify-center
-                "
+                class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
               ></div>
               <button
                 @click="disableEdit()"
-                class="
-                  w-full
-                  flex
-                  items-center
-                  justify-center
-                  bg-gray-600
-                  text-white
-                  font-semibold
-                  rounded
-                  hover:bg-gray-700
-                  hover:text-white
-                  shadow-md
-                  py-2
-                  px-6
-                  inline-flex
-                  items-center
-                "
+                class="w-full flex items-center justify-center bg-gray-600 text-white font-semibold rounded hover:bg-gray-700 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
               >
                 <span class="justify-center">Voltar</span>
               </button>
@@ -440,7 +229,7 @@ import { companyService } from "../../../services";
 import { TheMask } from "vue-the-mask";
 import { BarLoader } from "@saeris/vue-spinners";
 import { required } from "vuelidate/lib/validators";
-
+import { isValidCPF, isValidCNPJ } from "../../../helpers/validations";
 export default {
   name: "CompanyForm",
   components: {
@@ -499,51 +288,87 @@ export default {
     },
     updateCompany() {
       this.$v.$touch();
-
       if (this.$v.company.fantasy_name.$invalid) {
         this.errors.fantasy_name = "ERROR";
       }
-
       if (this.$v.company.company_name.$invalid) {
         this.errors.company_name = "ERROR";
       }
-
       if (this.$v.company.cnpj.$invalid) {
         this.errors.cnpj = "ERROR";
       }
-
       if (this.$v.company.phone.$invalid) {
         this.errors.phone = "ERROR";
       }
-
       if (this.$v.$anyError == false) {
-        const data = {
-          fantasy_name: this.company.fantasy_name,
-          company_name: this.company.company_name,
-          cnpj: this.company.cnpj,
-          state_vat: this.company.state_vat,
-          city_vat: this.company.city_vat,
-          email: this.company.email,
-          phone: this.company.phone,
-        };
-        companyService
-          .updateCompany(data)
-          .then((response) => {
-            this.$toast.success(response.success_message, {
-              position: "bottom-right",
-              pauseOnHover: false,
-              showCloseButtonOnHover: true,
-              timeout: 2500,
-            });
-            this.disableEdit();
-            bus.$emit("updatedUser", true);
-          })
-          .catch((error) => {
-            console.log(error.response.data);
+        if (
+          !this.company.cnpj ||
+          (this.company.cnpj.length !== 14 && this.company.cnpj.length !== 11)
+        ) {
+          this.$toast.error("Preencha o campo de CPF/CNPJ corretamente!", {
+            position: "bottom-right",
+            pauseOnHover: false,
+            showCloseButtonOnHover: true,
+            timeout: 2500,
           });
+        } else if (
+          this.company.cnpj.length === 11 &&
+          isValidCPF(this.company.cnpj) === false
+        ) {
+          this.$toast.error("CPF inválido!", {
+            position: "bottom-right",
+            pauseOnHover: false,
+            showCloseButtonOnHover: true,
+            timeout: 2500,
+          });
+        } else if (
+          this.company.cnpj.length === 14 &&
+          isValidCNPJ(this.company.cnpj) === false
+        ) {
+          this.$toast.error("CNPJ inválido!", {
+            position: "bottom-right",
+            pauseOnHover: false,
+            showCloseButtonOnHover: true,
+            timeout: 2500,
+          });
+        } else if (
+          this.company.phone.length !== 10 &&
+          this.company.phone.length !== 11
+        ) {
+          this.$toast.error("Telefone inválido!", {
+            position: "bottom-right",
+            pauseOnHover: false,
+            showCloseButtonOnHover: true,
+            timeout: 2500,
+          });
+        } else {
+          const data = {
+            fantasy_name: this.company.fantasy_name,
+            company_name: this.company.company_name,
+            cnpj: this.company.cnpj,
+            state_vat: this.company.state_vat,
+            city_vat: this.company.city_vat,
+            email: this.company.email,
+            phone: this.company.phone,
+          };
+          companyService
+            .updateCompany(data)
+            .then((response) => {
+              this.$toast.success(response.success_message, {
+                position: "bottom-right",
+                pauseOnHover: false,
+                showCloseButtonOnHover: true,
+                timeout: 2500,
+              });
+              this.disableEdit();
+              bus.$emit("updatedUser", true);
+            })
+            .catch((error) => {
+              console.log(error.response.data);
+            });
+        }
       } else {
         document.getElementById("form").scrollIntoView();
-
         this.$toast.error("Preencha corretamente todos os campos", {
           position: "bottom-right",
           pauseOnHover: false,
