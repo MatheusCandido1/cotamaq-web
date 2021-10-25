5eb7af45212a14b4f0a2ae1f5cdea609809224f8
Pedro Arthur <pedro13arthur@gmail.com>
2021-10-25 16:24:57 -0300

    new-adjustment

--- src/components/Seller/Proposals/ProposalConfirm.vue
+++ src/components/Seller/Proposals/ProposalConfirm.vue
@@ -38,6 +38,16 @@
                   <p class="text-sm text-gray-800">
                    Tem certeza que deseja enviar esta proposta?
                   </p>
+
+                  <div v-if="shipping == 0" class="bg-yellow-200 mt-6 rounded-md p-3 flex flex-col items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-800 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                    </svg>
+                    <p class="text-sm text-gray-800 pt-2">
+                    Favor confirmar CEP de entrega e valor do frete,
+                    caso seja zero será considerado como gratuito.
+                    </p>
+                  </div>
                 </div>
                 
               </div>
@@ -77,7 +87,7 @@
 <script>
 export default {
   name: "ProposalConfirm",
-  props: ['notShowNewProposalBtn'],
+  props: ['notShowNewProposalBtn', 'shipping'],
   data() {
     return {
       disabled: false
