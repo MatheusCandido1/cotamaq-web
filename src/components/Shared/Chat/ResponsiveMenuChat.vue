4e0a60501e690ec8e1888444b24430aefa1e7dc6
Pedro Arthur <pedro13arthur@gmail.com>
2021-11-09 15:29:52 -0300

    menu-chat

--- src/components/Shared/Chat/ResponsiveMenuChat.vue
+++ src/components/Shared/Chat/ResponsiveMenuChat.vue
@@ -18,19 +18,31 @@
                 <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
               </svg>
             </div>
+
             <div class="ml-3 w-full">
               <div class="w-full items-center flex justify-between">
-                <h5 class="font-bold truncate w-40">{{ conversation.user }}</h5>
+                <h5 class="font-bold truncate w-64">{{ conversation.user }}</h5>
                 <p class="text-gray-500 text-sm">{{formatDate(conversation.lastMessageDateTime)}}</p>
               </div>
-              <span :class="conversation.lastMessageIsImage == 1 && 'flex items-center'">
-                <svg v-if="conversation.lastMessageIsImage == 1" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
-                <p v-if="conversation.lastMessageIsImage == 1" class="text-gray-500 text-sm truncate w-64">Imagem</p>
-                <p v-else class="text-gray-500 text-sm truncate w-48">{{conversation.lastMessage}}</p>
+
+              <span v-if="conversation.lastMessageIsImage == 1" class="flex items-center justify-between">
+                <span class="flex items-center">
+                  <p v-if="conversation.userSend == userId" class="text-gray-500 text-sm mr-1">Você: </p>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                  </svg>
+                  <p  class="text-gray-500 text-sm truncate w-48">Imagem</p>
+                </span>
+                <div v-if="conversation.notification === 1" class="flex items-center justify-center text-sm h-5 w-5 rounded-full bg-primary-main text-white">!</div>
               </span>
+
+              <span v-else  class="flex items-center justify-between">
+                <p class="text-gray-500 text-sm truncate w-48">{{conversation.userSend == userId ? `Você: ${conversation.lastMessage}` : `${conversation.lastMessage}`}}</p>
+                <div v-if="conversation.notification === 1" class="flex items-center justify-center text-sm h-5 w-5 rounded-full bg-primary-main text-white">!</div>
+              </span>
+
             </div>
+
           </div>
         </div>
       </span>
@@ -45,6 +57,7 @@ export default {
   data() {
     return {
       search: "",
+      userId: localStorage.getItem('user_id'),
     };
   },
   computed: {
