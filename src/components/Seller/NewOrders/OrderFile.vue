982ca684bbcf5bd806de853d3dc4c0f51d0f201a
Pedro Arthur <pedro13arthur@gmail.com>
2021-11-10 18:43:46 -0300

    files

--- src/components/Seller/NewOrders/OrderFile.vue
+++ src/components/Seller/NewOrders/OrderFile.vue
@@ -17,7 +17,7 @@
           <!--body-->
           <div class="relative p-6 flex-auto bg-white">
             <div class="md:flex  -mx-3">
-                <div class="md:w-1/3 w-full px-3 mb-5">
+                <div class="md:w-1/2 w-full px-3 mb-5">
                 <form @submit="submitPdf" enctype="multipart/form-data">
                  <section class="h-full overflow-auto w-full h-full flex flex-col">
                  <label for="pdf" class="text-sm font-semibold text-center text-gray-600 px-1 mb-2">Nota Fiscal (PDF)
@@ -66,59 +66,8 @@
             </div>
                   </div>
                 </div>
-
-
-                <div class="md:w-1/3 w-full  px-3 mb-5">
-                <form @submit="submitXml" enctype="multipart/form-data">
-                 <section class="h-full overflow-auto w-full h-full flex flex-col">
-                 <label for="xml" class="text-sm font-semibold text-center text-gray-600 px-1 mb-2">Nota Fiscal (XML)
-            <header class="border-dashed border-2 border-gray-400 py-8 flex flex-col justify-center items-center">
-               <div class="cursor-pointer mx-auto mb-2 bg-primary-lighter flex-shrink-0 flex items-center justify-center h-16 w-16 rounded-full sm:mx-0 sm:h-16 sm:w-16">
-            <i class="mdi mdi-xml text-white text-3xl"></i>
-          </div>
-              <p class="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
-                <span>Arraste e solte seu arquivo aqui ou <span class="font-bold"> Clique para escolher o arquivo</span></span>
-              </p>
-              <input @change="onChangeXml" accept="text/xml" id="xml" name="xml" type="file" class="hidden" />
-            </header>
-            </label>
-             <span class="flex items-center justify-center text-primary-main mb-1" v-if="filename.xml">{{filename.xml}}</span>
-            <div class="relative pt-1" v-if="progress.xml > 1 && progress.xml <= 100">
-  <div class="flex mb-2 items-center justify-between">
-    <div>
-      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-white bg-primary-main">
-        Enviando
-      </span>
-    </div>
-    <div class="text-right">
-      <span class="text-xs font-semibold inline-block text-primary-main">
-        {{progress.xml}}%
-      </span>
-    </div>
-  </div>
-  <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-white">
-    <div aria-valuemin="0" aria-valuemax="100" :aria-valuenow="progress.xml" :style="{width: progress.xml + '%'}" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-primary-darker"></div>
-  </div>
-</div>
-            
-                <div class="flex justify-center mb-2" v-if="errors.xml">
-                    <span class="text-xs text-red-500 font-semibold px-1 text-center">O arquivo deve ser do formato XML.</span>
-                </div>  
-            <!-- <button v-if="files.xml" :disabled="disabled" type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-main text-base font-medium text-white hover:bg-primary-darker focus:outline-none focus:ring-2">Enviar</button> -->
-            
-          </section>  
-                </form>  
-                 <div  v-if="newOrder.xml" class="p-6">
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Nota Fiscal (XML)</h1>
-            <p class="leading-relaxed mb-3">Você já anexou uma Nota Fiscal (XML). Caso enviei um novo arquivo, o arquivo atual será <span class="text-red-500 font-semibold">excluido e substituído.</span></p>
-            <div class="flex items-center flex-wrap ">
-              <i class="mdi mdi-xml text-primary-main mr-1"></i>
-              <a :href="newOrder.xml_path" target="_blank" class="text-primary-main inline-flex items-center md:mb-2 lg:mb-0">Ver Nota Fiscal (XML)
-              </a>
-            </div>
-                  </div>
-                </div>
- <div class="md:w-1/3 w-full  px-3 mb-5">
+             
+ <div class="md:w-1/2 w-full  px-3 mb-5">
                 <form @submit="submitBillet" enctype="multipart/form-data">
                  <section class="h-full overflow-auto w-full h-full flex flex-col">
                  <label for="billet" class="text-sm font-semibold text-center text-gray-600 px-1 mb-2">Boleto Bancário
@@ -181,7 +130,7 @@
               Cancelar
             </button>
             <button
-              v-if="files.pdf != null || files.xml != null || files.billet != null"
+              v-if="files.pdf != null || files.billet != null"
               type="button"
               @click="submitFiles"
               class="mb-3 md:mb-0 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-main text-base font-medium text-white hover:bg-primary-main focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-main sm:ml-3 sm:w-auto sm:text-sm"
@@ -208,24 +157,20 @@ export default {
       disabled: false,
       selectedOrder: JSON.parse(JSON.stringify(this.$props.order)),
       progress: {
-        xml: 0,
         pdf: 0,
         billet: 0,
       },
       errors: {
-        xml: null,
         pdf: null,
         billet: null,
       },
       files: {
         pdf: null,
         billet: null,
-        xml: null
       },
       filename: {
         pdf: null,
         billet: null,
-        xml: null
       },
       newOrder: {},
       loader: {
@@ -245,13 +190,10 @@ export default {
       if (this.files.pdf){
         this.submitPdf()
       }
-      if (this.files.xml){
-        this.submitXml()
-      }
       if (this.files.billet) {
         this.submitBillet()
       }
-      if (this.errors.pdf == null && this.errors.xml == null && this.errors.billet == null){
+      if (this.errors.pdf == null && this.errors.billet == null){
         this.$toast.success('Arquivo(s) enviado(s) com sucesso!', {
           position: "bottom-right",
           pauseOnHover: false,
@@ -274,82 +216,35 @@ export default {
     },
     close() {
       this.files.pdf = null
-      this.files.xml = null
       this.files.billet = null
       this.$emit("close");
     },
     onChangePdf(event) {
       this.files.pdf = event.target.files[0];
       this.filename.pdf = event.target.files[0].name;
-      this.files.xml = null
-      this.filename.xml = null
     },
     submitPdf() {
       this.progress.pdf = 0
-      /* event.preventDefault(); */
       this.disabled = true
       let data = new FormData();
       data.append('pdf', this.files.pdf);
       orderService.updatePdf(data, this.selectedOrder.id, event => {
             this.progress.pdf = Math.round((100* event.loaded) / event.total)
       }).then(() => {
-            /* this.$toast.success(response.success_message, {
-                position: "bottom-right",
-                pauseOnHover: false,
-                showCloseButtonOnHover: true,
-                timeout: 2500
-            }); */
             bus.$emit('updatedOrders', true);
             this.disabled = false
             this.getOrder();
-            this.files.pdf = ''
+            this.files.pdf = null
             this.progress.pdf = 0
-            this.filename.pdf = ''
+            this.filename.pdf = null
         }).catch((error) => {
             console.log(error.response.data)
             this.errors.pdf = error.response.data.message
             this.disabled = false
             this.getOrder();
-            this.files.pdf = ''
+            this.files.pdf = null
             this.progress.pdf = 0
-            this.filename.pdf = ''
-        })
-
-    },
-    onChangeXml(event) {
-      this.files.xml = event.target.files[0];
-      this.filename.xml = event.target.files[0].name;
-      this.files.pdf = null
-      this.filename.pdf = null
-    },
-    submitXml() {
-      /* event.preventDefault(); */
-      this.disabled = true
-      let data = new FormData();
-      data.append('xml', this.files.xml);
-      orderService.updateXml(data, this.selectedOrder.id, event => {
-            this.progress.xml = Math.round((100* event.loaded) / event.total)
-      }).then(() => {
-            /* this.$toast.success(response.success_message, {
-                position: "bottom-right",
-                pauseOnHover: false,
-                showCloseButtonOnHover: true,
-                timeout: 2500
-            }); */
-            bus.$emit('updatedOrders', true);
-            this.disabled = false
-            this.getOrder();
-            this.files.xml = ''
-            this.progress.xml = 0
-            this.filename.xml = ''
-        }).catch((error) => {
-            console.log(error.response.data)
-            this.errors.xml = error.response.data.message
-            this.disabled = false
-            this.getOrder();
-            this.files.xml = ''
-            this.progress.xml = 0
-            this.filename.xml = ''
+            this.filename.pdf = null
         })
 
     },
@@ -358,33 +253,26 @@ export default {
       this.filename.billet = event.target.files[0].name;
     },
     submitBillet() {
-      /* event.preventDefault(); */
       this.disabled = true
       let data = new FormData();
       data.append('billet', this.files.billet);
       orderService.updateBillet(data, this.selectedOrder.id, event => {
             this.progress.billet = Math.round((100* event.loaded) / event.total)
       }).then(() => {
-            /* this.$toast.success(response.success_message, {
-                position: "bottom-right",
-                pauseOnHover: false,
-                showCloseButtonOnHover: true,
-                timeout: 2500
-            }); */
             bus.$emit('updatedOrders', true);
             this.disabled = false
             this.getOrder();
-            this.files.billet = ''
+            this.files.billet = null
             this.progress.billet = 0
-            this.filename.billet = ''
+            this.filename.billet = null
         }).catch((error) => {
             console.log(error.response.data)
             this.errors.billet = error.response.data.message
             this.disabled = false
             this.getOrder();
-            this.files.billet = ''
+            this.files.billet = null
             this.progress.billet = 0
-            this.filename.billet = ''
+            this.filename.billet = null
         })
 
     }
