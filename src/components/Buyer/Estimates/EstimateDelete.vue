8587a913bc718825678d1a636bfd8511879c9ded
Pedro Arthur <pedro13arthur@gmail.com>
2021-12-08 20:43:00 -0300

    rename-modal-delete

--- src/components/Buyer/Estimates/EstimateDelete.vue
+++ src/components/Buyer/Estimates/EstimateDelete.vue
@@ -64,14 +64,14 @@
               :disabled="disabled"
               class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
             >
-              {{selectedEstimate.status == 5 ? 'Apagar':'Cancelar'}}
+              {{selectedEstimate.status == 5 ? 'Apagar':'Sim'}}
             </button>
             <button
               type="button"
               @click="close"
               class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-main sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
-              Cancelar
+              {{selectedEstimate.status == 5 ? 'Cancelar':'Não'}}
             </button>
           </div>
         </div>
--- src/components/Seller/Proposals/ProposalDecline.vue
+++ src/components/Seller/Proposals/ProposalDecline.vue
@@ -55,14 +55,14 @@
               :disabled="disabled"
               class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-lighter sm:ml-3 sm:w-auto sm:text-sm"
             >
-              Cancelar
+              Sim
             </button>
             <button
               type="button"
               @click="close"
               class="mt-3 w-full inline-flex justify-center rounded-md border  shadow-sm px-4 py-2 bg-gray-500 text-white text-base font-medium hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-main sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
-              Fechar
+              Não
             </button>
           </div>
         </div>
